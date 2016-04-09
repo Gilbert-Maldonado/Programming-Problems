9a6e30de89ec660bdf3c04109126cc2c56057465
gilbert-maldonado <gilbert_maldonado@utexas.edu>
2016-04-08 22:30:17 -0500

    easy problem

--- HackerRank-Problems/Random/TwoStrings.java
+++ HackerRank-Problems/Random/TwoStrings.java
@@ -13,11 +13,31 @@ public class TwoStrings {
     }
 
     public static void computeStrings(int n, Scanner kb) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
 
         for(int i = 0; i < n; i++) {
             String first = kb.nextLine();
-            String second = kb.nextLine();
-            
+            String second = kb.nextLine();    
+
+            for(int j = 0; j < first.length(); j++) {
+                if(map.containsKey(first.charAt(j))) {
+                    map.put(first.charAt(j), map.get(first.charAt(j)) + 1);
+                } else {
+                    map.put(first.charAt(j), 1);
+                }
+            }   
+
+            boolean found = false;
+            int index = 0;
+            while(!found && index < second.length()) {
+                if(map.get(second.charAt(index)) != null) {
+                    found = true;
+                }
+                index++;
+            }
+            System.out.println(found ? "YES" : "NO");
+            map.clear();
         }
+
     }
 }
