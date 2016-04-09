853efb2578ec676d57f619d8a60568416de2b698
gilbert-maldonado <gilbert_maldonado@utexas.edu>
2016-04-09 00:10:59 -0500

    Started problem

---
+++ HackerRank-Problems/Random/CounterGame.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class CounterGame {
+
+    public static void main(String [] args) {
+        Scanner kb = new Scanner(System.in);
+        
+    }
+}
