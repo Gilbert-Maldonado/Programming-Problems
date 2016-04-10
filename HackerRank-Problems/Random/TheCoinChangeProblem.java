828da029331ad143556074e5eb8e844ada3141b9
gilbert-maldonado <gilbert_maldonado@utexas.edu>
2016-04-10 00:17:30 -0500

    Starting next problem

---
+++ HackerRank-Problems/Random/TheCoinChangeProblem.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class TheCoinChangeProblem {
+
+    public static void main(String [] args) {
+        Scanner kb = new Scanner(System.in);
+         
+    }
+}
