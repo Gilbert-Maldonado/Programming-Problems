2ba06b47f9a8f60c9f7d59fd35465dee48032a89
gilbert-maldonado <gilbert_maldonado@utexas.edu>
2016-04-10 00:27:21 -0500

    Leaving

--- HackerRank-Problems/Random/TheCoinChangeProblem.java
+++ HackerRank-Problems/Random/TheCoinChangeProblem.java
@@ -1,10 +1,22 @@
 import java.util.*;
 import java.io.*;
 
+// https://www.hackerrank.com/challenges/coin-change
 public class TheCoinChangeProblem {
 
     public static void main(String [] args) {
         Scanner kb = new Scanner(System.in);
-         
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] coins = new int[m];
+        fillCoinsArr(kb, coins);
+        kb.close();
+    }
+
+    public static void fillCoinsArr(Scanner kb, int[] coins) {
+
+        for(int i = 0; i < coins.length; i++) {
+            coins[i] = kb.nextInt();
+        }
     }
 }
