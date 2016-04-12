a24ebf04bfbe3bcdece3a2718f6c30e2c2a398b2
gilbert-maldonado <gilbert_maldonado@utexas.edu>
2016-04-12 00:09:23 -0500

    This problem might be hard

--- HackerRank-Problems/Random/CommonChild.java
+++ HackerRank-Problems/Random/CommonChild.java
@@ -1,9 +1,14 @@
 import java.io.*;
 import java.util.*;
 
+// https://www.hackerrank.com/challenges/common-child
 public class CommonChild {
 
     public static void main(String [] args) {
-        
+        Scanner kb = new Scanner(System.in);
+        String a = kb.nextLine();
+        String b = kb.nextLine();
+        kb.close();
+
     }
 }
