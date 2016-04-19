edd5018a221f542e3a5edca62142836b8d1dce75
gilbert-maldonado <gilbert_maldonado@utexas.edu>
2016-04-19 00:14:52 -0500

    .

--- HackerRank-Problems/Random/CommonChild.java
+++ HackerRank-Problems/Random/CommonChild.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 // https://www.hackerrank.com/challenges/common-child
+// https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
 public class CommonChild {
 
     public static void main(String [] args) {
@@ -9,7 +10,7 @@ public class CommonChild {
         String a = kb.nextLine();
         String b = kb.nextLine();
         int[][] arr = new int[a.length][b.length];
-        
+
         kb.close();
 
     }
