6824dfef5f81728f9848a2c74f9df527a05d5fee
Gilbert Maldonado <gilbert_maldonado@utexas.edu>
2016-05-08 22:39:14 -0500

    Started new problem

---
+++ HackerRank-Problems/Random/SteppingStonesGame.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+// https://www.hackerrank.com/challenges/stepping-stones-game
+public class SteppingStonesGame {
+
+	public static void main(String [] args) {
+		Scanner kb = new Scanner(System.in);
+		int tests = kb.nextInt();
+		for(int i = 0; i < tests; i++) {
+			playGame(kb);
+		}
+	}
+
+	public static void playGame(Scanner kb) {
+		
+	}
+}
