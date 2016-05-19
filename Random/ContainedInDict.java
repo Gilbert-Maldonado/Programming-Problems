d22debda30e31a7e334a384dd2dcc193e73993b3
Gilbert Maldonado <gilbert_maldonado@utexas.edu>
2016-05-18 23:50:33 -0500

    Started a problem to see if word is in the dictionary, or substrings of that word

---
+++ Random/ContainedInDict.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+// Checks to see if
+public class ContainedInDict {
+
+	public static void main(String [] args) {
+
+		Set<String> dict = new HashSet<String>();
+
+	}
+
+	public static boolean isWord(Set<String> dict) {
+
+	}
+}
